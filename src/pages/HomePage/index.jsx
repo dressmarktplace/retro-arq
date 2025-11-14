eff91fb6a457f8db987e19ab7f21dd68c309baaa
MichelleSilva <dressmarktplace@gmail.com>
2025-11-13 21:21:36 -0300

    Implementado o componente navbar

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,8 +1,10 @@
 import { Footer } from "../../components/Footer"
+import { Navbar } from "../../components/Navbar"
 
 export const HomePage = () => {
     return (
         <div className="w-full">
+            <Navbar />
             <Footer />
         </div>
     )
